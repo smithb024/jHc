7881e3b6eafea8408c4c39e0a9a94ce9b8ad530d
Bert Smith <bertram.smith@gmail.com>
2021-12-28 17:54:36 +0000

    #14: Create the message classes.

---
+++ CommonHandicapLib/Messages/LoadNewEventMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace CommonHandicapLib.Messages
+{
+    /// <summary>
+    /// Message class. This is used when a new event has been selected and is to be loaded
+    /// into the model.
+    /// </summary>
+    public class LoadNewEventMessage
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LoadNewEventMessage"/> class.
+        /// </summary>
+        /// <param name="seasonName">event name</param>
+        public LoadNewEventMessage(string eventName)
+        {
+            this.Name = eventName;
+        }
+
+        /// <summary>
+        /// Gets the name of the new season.
+        /// </summary>
+        public string Name { get; }
+    }
+}
---
+++ CommonHandicapLib/Messages/LoadNewSeasonMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace CommonHandicapLib.Messages
+{
+    /// <summary>
+    /// Message class. This is used when a new season has been selected and is to be loaded
+    /// into the model.
+    /// </summary>
+    public class LoadNewSeasonMessage
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LoadNewSeasonMessage"/> class.
+        /// </summary>
+        /// <param name="seasonName">season name</param>
+        public LoadNewSeasonMessage(string seasonName)
+        {
+            this.Name = seasonName;
+        }
+
+        /// <summary>
+        /// Gets the name of the new season.
+        /// </summary>
+        public string Name { get; }
+    }
+}
