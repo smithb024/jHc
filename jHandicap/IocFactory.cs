0f0b08f054b07c94c541f254a4edbc2e1781411f
smithb024 <bertram.smith@gmail.com>
2024-09-07 16:08:00 +0100

    #96: Set up dependency injection.

---
+++ jHandicap/IocFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace jHandicap
+{
+    using CommunityToolkit.Mvvm.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Factory class, used to set up dependency injection
+    /// </summary>
+    public class IocFactory
+    {
+        /// <summary>
+        /// Setup IOC.
+        /// </summary>
+        public static void Setup()
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                .AddSingleton<IMainWindowViewModel, MainWindowViewModel>()
+                .BuildServiceProvider());
+        }
+    }
+}
